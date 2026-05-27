8cdab2fedccde7393e9f701fb678680eb99b5cdd
AbhijeetMad <abhijeetmadhukar2004@outlook.com>
2026-05-27 19:30:58 +0530

    Add files via upload

---
+++ Code001.m
@@ -0,0 +1,7 @@
+X= 'hello world';
+Y= "Welocome to Matlab";
+Z='Abhijeet this side';
+disp(X);
+disp(Y);
+disp(Z);
+
---
+++ Code002.m
@@ -0,0 +1,18 @@
+num1 = input('Enter the first number: ');
+num2 = input('Enter the second number: ');
+
+% Addition
+add_result = num1 + num2;
+fprintf('Addition of %.2f and %.2f is: %.2f\n', num1, num2, add_result);
+
+% Subtraction
+sub_result = num1 - num2;
+fprintf('Subtraction (%.2f - %.2f) is: %.2f\n', num1, num2, sub_result);
+
+% Multiplication
+mul_result = num1 * num2;
+fprintf('Multiplication of %.2f and %.2f is: %.2f\n', num1, num2, mul_result);
+
+
+
+
